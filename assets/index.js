fd53366c98a0da9a5d0250b17fda89adbbf8009f
dIANE phan <dianephan12@gmail.com>
2020-12-15 19:27:41 -0800

    create new functions on index.js to render video nicely

--- assets/index.js
+++ assets/index.js
@@ -1,39 +1,47 @@
 (() => {
     'use strict';
     const TWILIO_DOMAIN = location.host; 
-    //unique to user, will be website to visit for video app
     const ROOM_NAME = 'myfirstvideoapp';
     const Video = Twilio.Video;
     let videoRoom, localStream;
     const video = document.getElementById("video");
    
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then(vid => {
-        video.srcObject = vid;
-        localStream = vid;
-    })
+    // preview video in nav doesn't show up bc this is commented out 
+    // navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    // .then(vid => {
+    //     video.srcObject = vid;
+    //     localStream = vid;
+    // })
 
     const joinRoomButton = document.getElementById("button-join");
     const leaveRoomButton = document.getElementById("button-leave");
-    // var site = `https://${TWILIO_DOMAIN}/generatetoken`;
-    // console.log(`site ${site}`);
+
     joinRoomButton.onclick = () => {
       // get access token
       axios.get(`https://${TWILIO_DOMAIN}/generatetoken`).then(async (body) => {
         const token = body.data.token;
-        // console.log(token);
-        Video.connect(token, { name: ROOM_NAME }).then((room) => {
+        Video.connect(token, { 
+          name: ROOM_NAME,
+          video: true,
+          audio: true
+        }).then((room) => {
           // console.log(`Connected to Room ${room.name}`);
           videoRoom = room;
-          room.participants.forEach(participantConnected);
-          room.on("participantConnected", participantConnected);
-          room.on("participantDisconnected", participantDisconnected);
-          room.once("disconnected", (error) =>
-            room.participants.forEach(participantDisconnected)
-          );
           joinRoomButton.disabled = true;
           leaveRoomButton.disabled = false;
-        });
+
+          //loads all existing participants ((NEW LINES))
+          addLocalParticipant(room.localParticipant)
+          room.participants.forEach(participant => participantConnected(participant));
+
+          // when new participant connects or disconnects          
+          room.on("participantConnected", participantConnected);
+          room.on("participantDisconnected", participantDisconnected);
+
+            room.once("disconnected", (error) =>
+              room.participants.forEach(participantDisconnected)
+            );
+          });
       });
     };
     leaveRoomButton.onclick = () => {
@@ -44,22 +52,17 @@
     };
 })();
 
-const participantConnected = (participant) => {
-  console.log(`Participant ${participant.identity} connected'`);
-
-  const div = document.createElement('div'); //create div for new participant
-  div.id = participant.sid;
-  div.classList.add('remoteParticipant')
-  
-  participant.on('trackSubscribed', track => trackSubscribed(div, track));
+const participantConnected = (participant, local=false) => {
+  participant.on('trackSubscribed', track => addTrack(participant.sid, track));
   participant.on('trackUnsubscribed', trackUnsubscribed);
+}
 
+// new function 
+const addLocalParticipant = participant => {
+  console.log('adding local participant')
   participant.tracks.forEach(publication => {
-    if (publication.isSubscribed) {
-      trackSubscribed(div, publication.track);
-    }
+      addTrack(participant.sid, publication.track)
   });
-  document.body.appendChild(div);
 }
 
 const participantDisconnected = (participant) => {
@@ -69,6 +72,16 @@ const participantDisconnected = (participant) => {
 
 const trackSubscribed = (div, track) => {
   div.appendChild(track.attach());
+}
+
+// new function 
+const addTrack = (sid, track) => {
+  console.log(track)
+  const div = document.createElement('div'); // create div for new participant
+  div.id = sid;
+  div.classList.add('remoteParticipant');
+  div.appendChild(track.attach());
+  document.body.appendChild(div);
 }
 
 const trackUnsubscribed = (track) => {
